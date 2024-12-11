53ef3406fec51ba69d6fc98481d0367323374dbd
martijnbeeks <49335034+martijnbeeks@users.noreply.github.com>
2024-12-11 11:48:20 +0100

    Add logo

--- app/main.py
+++ app/main.py
@@ -4,17 +4,68 @@ import plotly.graph_objects as go
 from PIL import Image
 from matplotlib import pyplot as plt
 import numpy as np
+import base64
 
+# Function to read and encode the image in base64
+def get_base64_image(image_path):
+    with open(image_path, "rb") as img_file:
+        data = img_file.read()
+    encoded = base64.b64encode(data).decode()
+    return encoded
 
-col1, col2 = st.columns([1, 5])
-with col1:
-    st.image("app/high_eye.jpeg", width=100)
-with col2:
-    st.markdown("<h1 style='text-align: center; color: grey;'>Payload distributor</h1>", unsafe_allow_html=True)
+image_path = 'app/higheye_logo.png'  # Replace 'logo.png' with your image file path
 
-# Sidebar for inputs
+# Encode the image
+image_base64 = get_base64_image(image_path)
+
+banner_html = f"""
+<style>
+.banner {{
+    background-color: rgb(38, 89, 128);
+    padding: 10px;
+    display: flex;
+    align-items: center;
+}}
+.banner img {{
+    max-height: 50px;
+    margin-right: 20px;
+}}
+.banner-text {{
+    color: white;
+    font-size: 24px;
+    text-align: center;
+    flex-grow: 1;
+}}
+</style>
+<div class="banner">
+    <img src="data:image/png;base64,{image_base64}" alt="Logo">
+    <div class="banner-text">Payload distributor</div>
+</div>
+"""
+
+st.markdown(banner_html, unsafe_allow_html=True)
 with st.sidebar:
-    st.write("Payload details")
+    banner_html = f"""
+    <style>
+    .banner {{
+        background-color: rgb(38, 89, 128);
+        padding: 10px;
+        display: flex;
+        align-items: center;
+    }}
+    .banner img {{
+        max-height: 50px;
+        margin-right: 20px;
+    }}
+    </style>
+    <div class="banner">
+        <img src="data:image/png;base64,{image_base64}" alt="Logo">
+        <div class="banner-text"> </div>
+    </div>
+    """
+    st.write("")
+    st.markdown(banner_html, unsafe_allow_html=True)
+    st.header("Payload details")
     weight = st.number_input('Weight (kg)', min_value=0.0, format="%.2f")
     arm = st.number_input('Arm (m)', format="%.2f")
 
@@ -22,12 +73,11 @@ with st.sidebar:
         st.session_state.weight = weight
         st.session_state.arm = arm
     
-    st.header('Input Table')
+    st.write(f"**Input Table**")
     st.write('Add rows with **Weight** and **Arm** values:')
-    # Initialize the DataFrame in session state if not already
 
     st.session_state.df = pd.DataFrame({
-        'Component': ['Tail Gearbox', 'Engine', 'Exhaust', 'Heli', 'Payload'],
+        'Component': ['Tail Gearbox', 'Engine', 'Exhaust', 'Heli', "Payload"],
         'Weight (kg)': [0.32, 4.7, 0.78, 26, weight],
         'Arm (m)': [1.12, -0.05, 0.43, 0.09, arm]
     })
@@ -46,6 +96,7 @@ center_of_gravity = total_moment / total_weight * 1000 if total_weight != 0 else
 
 
 # Display calculation results in columns
+st.write("")
 col1, col2, col3 = st.columns(3)
 col1.metric(label="Total Weight (kg)", value=f"{total_weight:.2f}")
 col2.metric(label="Total Moment (m*Kg)", value=f"{total_moment:.2f}")
@@ -86,71 +137,52 @@ fig = go.Figure()
 fig.add_trace(
     go.Image(
         z=image1_array,
-        opacity=1.0  # Full opacity
+        opacity=1.0 
     )
 )
 
-# Add the second image with slight transparency and a slight offset for the overlay
 fig.add_trace(
     go.Image(
         z=image2_array,
-        opacity=0.6,  # Slight transparency
-        dx=0.4,  # Adjust x position
-        dy=0.4,   # Adjust y position
-        x0=650+move_distance,  # Adjust x position
-        y0=217   # Adjust y position
+        opacity=0.6,
+        dx=0.4,
+        dy=0.4,
+        x0=650+move_distance,
+        y0=217
     ),
 )
 
-# Add the third image with slight transparency and a slight offset for the overlay
 fig.add_trace(
     go.Image(
         z=image3_array,
-        # opacity=0.6,  # Slight transparency
-        dx=.04,  # Adjust x position
-        dy=.04,   # Adjust y position
-        x0=300,  # Adjust x position
-        y0=217     # Adjust y position
+        dx=.04,
+        dy=.04,
+        x0=300,
+        y0=217
     ),
 )
 
-# Add the fourth image with slight transparency and a slight offset for the overlay
 fig.add_trace(
     go.Image(
         z=image3_array,
-        # opacity=0.6,  # Slight transparency
-        dx=.04,  # Adjust x position
-        dy=.04,   # Adjust y position
-        x0=692+move_distance,  # Adjust x position
-        y0=220     # Adjust y position
+        dx=.04,
+        dy=.04,
+        x0=692+move_distance,
+        y0=220
     ),
 )
 
 # Add a line between the two images
 fig.add_shape(
     type="line",
-    x0=300,
+    x0=330,
     y0=233,
     x1=692+move_distance,
     y1=233,
     line=dict(
-        color="blue",
+        color="red",
         width=2,
     ))
-
-# Add small leged with moved distance
-# fig.add_annotation(
-#     x=0.5,
-#     y=0.9,
-#     xref="paper",
-#     yref="paper",
-#     text=move_text,
-#     showarrow=False,
-#     font=dict(
-#         size=16,
-#         color="black"
-#     )
-# )
 
 # Add annotation
 fig.add_annotation(
